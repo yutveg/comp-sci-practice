34742920e85aef6231550d72e4259c2bc7369f5a
yutveg <kodi.0n31l@gmail.com>
2020-09-15 13:32:44 -0700

    reviewed and comment on median of two sorted arrays

--- arrays/medianTwoSorted.js
+++ arrays/medianTwoSorted.js
@@ -13,22 +13,32 @@ var findMedianSortedArrays = (A, B) => {
 };
 
 var findMedianSortedArrays = function (nums1, nums2) {
+  // flipping arrays so that nums1 is smallest or they are equal
   if (nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
   let x = nums1.length;
   let y = nums2.length;
   let low = 0,
     high = x;
   while (low <= high) {
+    // Initializing partition X by halving the value and flooring any decimals
     const partitionX = (high + low) >> 1;
+    // Initialize partition Y by halving the combined lengths (+1) and flooring, offset by the partition X
+
+    // we offset by partition X so that the length of the combined elements of minX and minY are equal to (or +1 greater than)
+    // the combined elements of maxX and maxY
     const partitionY = ((x + y + 1) >> 1) - partitionX;
 
+    // Grabbing our value left of the partition X
     const maxX =
       partitionX == 0 ? Number.NEGATIVE_INFINITY : nums1[partitionX - 1];
+    // Grabbing our value left of the partition Y
     const maxY =
       partitionY == 0 ? Number.NEGATIVE_INFINITY : nums2[partitionY - 1];
 
+    // Grabbing our value right of the partition X
     const minX =
       partitionX == nums1.length ? Number.POSITIVE_INFINITY : nums1[partitionX];
+    // Grabbing our value right of the partition Y
     const minY =
       partitionY == nums2.length ? Number.POSITIVE_INFINITY : nums2[partitionY];
 
