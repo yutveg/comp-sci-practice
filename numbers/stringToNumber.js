bfe66177518d00a9807b4222c0da1fcd7360df7d
yutveg <kodi.0n31l@gmail.com>
2020-09-15 14:58:18 -0700

    brute force string to number solution

--- numbers/stringToNumber.js
+++ numbers/stringToNumber.js
@@ -7,23 +7,31 @@ var myAtoi = function (str) {
   let stringNumber = "";
 
   for (let i = 0; i < str.length; i++) {
-    // we hit a negative sign before our number has been set
-    if (str[i] === "-" && stringNumber.length === 0) {
+    // we hit a negative/positive sign before our number has been set
+    if (
+      (str[i] === "+" && stringNumber.length === 0) ||
+      (str[i] === "-" && stringNumber.length === 0)
+    ) {
       stringNumber = stringNumber.concat(str[i]);
     }
 
     // we hit a number
-    else if (isNaN(str[i]) === false) {
+    else if (Number.isNaN(parseInt(str[i], 10)) === false) {
       stringNumber = stringNumber.concat(str[i]);
     }
 
     // we hit a letter before hitting a number
-    else if (isNaN(str[i]) === true && stringNumber.length === 0) {
+    else if (
+      str[i] !== " " &&
+      Number.isNaN(parseInt(str[i], 10)) === true &&
+      stringNumber.length === 0
+    ) {
       break;
     }
     // we hit a whitespace/letter after retrieving our number
-    if (
-      (isNaN(str[i]) === true && stringNumber.length > 0) ||
+    else if (
+      (Number.isNaN(parseInt(str[i], 10)) === true &&
+        stringNumber.length > 0) ||
       (str[i] === " " && stringNumber.length > 0)
     ) {
       break;
@@ -31,14 +39,19 @@ var myAtoi = function (str) {
   }
 
   // if conversion couldn't happen
-  if (stringNumber === "" || stringNumber === "-") {
+  if (stringNumber === "" || stringNumber === "-" || stringNumber === "+") {
     return 0;
   }
 
   // if we can convert
   else {
-    return parseInt(stringNumber, 10);
+    let finalNum = parseInt(stringNumber, 10);
+    if (finalNum >= 2147483647) {
+      return 2147483647;
+    } else if (finalNum <= -2147483648) {
+      return -2147483648;
+    }
+
+    return finalNum;
   }
 };
-
-console.log(myAtoi("   -42"));
