69db1b959358e3df54be267239ba23dfebc0f8cc
yutveg <kodi.0n31l@gmail.com>
2020-09-17 15:36:46 -0700

    logical proof of algorithm

--- mathRelated/maxAreaFromArray.js
+++ mathRelated/maxAreaFromArray.js
@@ -11,8 +11,19 @@ function maxArea(height) {
     );
 
     // checking which line is smaller and thus, potentially bottlenecking our max
-    if (height[left] < height[right]) l++;
+    if (height[left] < height[right]) left++;
     else right--;
   }
   return maxarea;
 }
+
+// the reason this algorithm works is simple
+
+// P.1) we have a height, and a width
+
+// P.2) our height is capped by the shortest height (left or right)
+
+// P.3) reducing our width while height stays the same will decrease our max area
+
+// C.) moving off of our larger height value will always decrease the max area
+// because our height will by definition stay the same and width will decrease
