5007c6c7fa7ec2101f1d1460f8ab79b5b0dad1d7
yutveg <kodi.0n31l@gmail.com>
2020-10-15 15:34:20 -0700

    proactive optimization attempt to roman to int

--- strings/romanToInt.js
+++ strings/romanToInt.js
@@ -1,3 +1,5 @@
+// retroactive correction
+
 function romanNumeralToInteger(s) {
   if (s.length < 1) return null;
 
@@ -30,6 +32,46 @@ function romanNumeralToInteger(s) {
       integerValue += valueLookUp[currentCharacter];
     }
     previousCharacter = currentCharacter;
+  }
+
+  return integerValue;
+}
+
+// proactive adjustments
+function romanNumeralToInteger2(s) {
+  if (s.length < 1) return null;
+
+  let valueLookUp = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  // lookahead type of way
+  // keep a current, and a lookahead character
+  // if current is less than lookahead, add difference to integer value
+  // if current is >= lookahead, add current to integer value
+  //
+
+  let integerValue = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let currentValue = valueLookUp[s[i]];
+    let lookaheadValue = valueLookUp[s[i + 1]];
+
+    // have we found a subtraction pair?
+    if (lookaheadValue && currentValue < lookaheadValue) {
+      integerValue += lookaheadValue - currentValue;
+      i++;
+    }
+    // we have not found a subtraction pair
+    else {
+      integerValue += currentValue;
+    }
   }
 
   return integerValue;
