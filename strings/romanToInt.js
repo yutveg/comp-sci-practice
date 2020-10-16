731fc75cd428f25aebc69792f8ec6bdaf3b74f6d
yutveg <kodi.0n31l@gmail.com>
2020-10-16 12:18:19 -0700

    alternative solution from leetcode

--- strings/romanToInt.js
+++ strings/romanToInt.js
@@ -76,3 +76,37 @@ function romanNumeralToInteger2(s) {
 
   return integerValue;
 }
+
+const romanToInt = (s) => {
+  if (!s || s.length === 0) {
+    return 0;
+  }
+
+  const map = new Map([
+    ["I", 1],
+    ["V", 5],
+    ["X", 10],
+    ["L", 50],
+    ["C", 100],
+    ["D", 500],
+    ["M", 1000],
+  ]);
+
+  let i = s.length - 1;
+  let result = map.get(s[i]);
+
+  while (i > 0) {
+    const curr = map.get(s[i]);
+    const prev = map.get(s[i - 1]);
+
+    if (prev >= curr) {
+      result += prev;
+    } else {
+      result -= prev;
+    }
+
+    i--;
+  }
+
+  return result;
+};
