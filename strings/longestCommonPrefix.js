e988c32547eb58a1272e22f3538433486ea4cdf1
yutveg <kodi.0n31l@gmail.com>
2020-10-19 11:27:49 -0700

    a more readable solution

--- strings/longestCommonPrefix.js
+++ strings/longestCommonPrefix.js
@@ -34,4 +34,18 @@ var longestCommonPrefix = function (strs) {
   return prefix;
 };
 
+function longestCommonPrefix(strs) {
+  if (!strs.length) return "";
+
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let str of strs) {
+      if (str[i] !== strs[0][i]) {
+        return str.slice(0, i);
+      }
+    }
+  }
+
+  return strs[0];
+}
+
 console.log(longestCommonPrefix(["flower", "flow", "flight"]));
