18cac21680836e58570910598afbcb51a1b7c198
yutveg <kodi.0n31l@gmail.com>
2020-10-19 10:52:29 -0700

    laying out solution for longest common prefix

---
+++ strings/longestCommonPrefix.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  // takes in an array of strings
+  // find longest common prefix
+
+  // if no common prefix return none
+
+  let prefix = "";
+
+  // as we loop we do two things:
+  // 1.) we need to add a character to prefix
+  // 2.) check that character against other strings
+  // -- if same continue
+  // -- if conflict stop
+
+  while (true) {}
+
+  return prefix;
+};
