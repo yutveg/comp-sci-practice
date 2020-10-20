331744056f6fb864b0520c6296886c5eade6618f
yutveg <kodi.0n31l@gmail.com>
2020-10-20 13:15:07 -0700

    organized and commented the threeSum window solution

--- numbers/threeSum.js
+++ numbers/threeSum.js
@@ -1,55 +1,3 @@
-var threeSum = function (nums) {
-  // Utilize a hashtable as a quick look up
-  // with number as key and its index as the value
-  let lookup = {};
-  for (let i = 0; i < nums.length; i++) {
-    lookup[nums[i]] = i;
-  }
-
-  let solutionArray = [];
-
-  // Two runners go through array
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = 0; j < nums.length; j++) {
-      if (i === nums.length - 1 && j === nums.length - 1) {
-        break;
-      } else if (i === j) {
-        continue;
-      }
-
-      let requiredValue = (nums[i] + nums[j]) * -1;
-
-      // We found a three sum
-      if (
-        lookup[requiredValue] &&
-        lookup[requiredValue] !== i &&
-        lookup[requiredValue] !== j
-      ) {
-        let sortedThreeSum = [nums[i], nums[j], requiredValue].sort(
-          (a, b) => a - b
-        );
-
-        // We found a unique three sum
-        if (!isArrayInMatrix(solutionArray, sortedThreeSum)) {
-          solutionArray.push(sortedThreeSum);
-        }
-      }
-    }
-  }
-
-  return solutionArray;
-};
-
-function isArrayInMatrix(arr, query) {
-  let queryString = JSON.stringify(query);
-
-  let contains = arr.some((item) => {
-    return JSON.stringify(item) === queryString;
-  });
-
-  return contains;
-}
-
 // fast solution
 
 var threeSum = function (nums) {
@@ -61,26 +9,34 @@ var threeSum = function (nums) {
     return a - b;
   });
   for (var i = 0; i < nums.length - 2; i++) {
+    // once sorted if no negatives exist then no possible way to sum to zero
     if (nums[i] > 0) {
       return rtn;
     }
+    // if the number behind is same as current we would've captured its pair already so skip
     if (i > 0 && nums[i] == nums[i - 1]) {
       continue;
     }
     for (var j = i + 1, k = nums.length - 1; j < k; ) {
+      // we find a unique pair, so narrow search window
       if (nums[i] + nums[j] + nums[k] === 0) {
         rtn.push([nums[i], nums[j], nums[k]]);
         j++;
         k--;
+        // find our new unique values for j and k
         while (j < k && nums[j] == nums[j - 1]) {
           j++;
         }
         while (j < k && nums[k] == nums[k + 1]) {
           k--;
         }
-      } else if (nums[i] + nums[j] + nums[k] > 0) {
+      }
+      // if our sum is too big, shrink right side of window
+      else if (nums[i] + nums[j] + nums[k] > 0) {
         k--;
-      } else {
+      }
+      // if our sum is too small, shrink left side of window
+      else {
         j++;
       }
     }
